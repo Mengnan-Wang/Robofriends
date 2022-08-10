a48c8a8e08be8a37945a5b68993e4ee865556f4d
mengnan_wang <megan.202631@gmail.com>
2022-08-10 13:15:32 +1000

    deploy

---
+++ build/precache-manifest.3936e0ae144395ae3932188a56facc88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ffdbbf20c24a92ac19a",
+    "url": "/Robofriends/static/css/main.a5b04cde.chunk.css"
+  },
+  {
+    "revision": "5ffdbbf20c24a92ac19a",
+    "url": "/Robofriends/static/js/main.5ffdbbf2.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/Robofriends/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "0f2fab6b06d617829c90",
+    "url": "/Robofriends/static/css/2.0e1247c2.chunk.css"
+  },
+  {
+    "revision": "0f2fab6b06d617829c90",
+    "url": "/Robofriends/static/js/2.0f2fab6b.chunk.js"
+  },
+  {
+    "revision": "24a68c757de214038306",
+    "url": "/Robofriends/static/js/runtime~main.24a68c75.js"
+  },
+  {
+    "revision": "2a69819ff1dfd9b8e304ac1ad42d04d0",
+    "url": "/Robofriends/index.html"
+  }
+];
